1c2f7aab4c0e1dfb00aa026a73264970a1363b6a
Matthéo Lécrivain <mattheo9912@protonmail.com>
2024-03-25 15:36:35 -0400

    Adjusted SVGs to fit the parent div correctly.

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -54,7 +54,7 @@ const CustomCard = ({ data, index }: CustomCardProps) => {
 					{data.skills && (
 						<div className="flex space-x-2">
 							{data.skills.map((skillsItem) => (
-								<Chip className="bg-slate-300 bg-opacity-50 dark:bg-slate-700">{skillsItem}</Chip>
+								<Chip className="bg-slate-300 bg-opacity-50 dark:bg-sky-950">{skillsItem}</Chip>
 							))}
 						</div>
 					)}
