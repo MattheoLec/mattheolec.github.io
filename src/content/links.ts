fa117fe0ae1fdb9a5ab453a03322c17165b6a4da
Matthéo Lécrivain <mattheo9912@protonmail.com>
2024-06-11 09:09:00 -0400

    Update resume link.

--- src/content/links.ts
+++ src/content/links.ts
@@ -12,6 +12,6 @@ export default {
 		short: "mattheo.lecrivain@tuta.io",
 	},
 	Resume: {
-		full: "https://media.licdn.com/dms/document/media/D4E2DAQESmPLmGnIFXg/profile-treasury-document-pdf-analyzed/0/1716389646024?e=1717027200&v=beta&t=emP6ghqmCPgBs_i3jl3Pe5L8klMtlZeBLk3N1KOHT9c",
+		full: "/Resume_Lecrivain.pdf",
 	},
 };
